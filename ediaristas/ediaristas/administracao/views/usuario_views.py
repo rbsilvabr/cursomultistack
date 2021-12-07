d69414c6fcf5dcf7241819b0540534f4dce5b355
Rodrigo Belo <rodrigobelosilva@gmail.com>
2021-12-06 22:52:44 -0300

    Listagem de usuarios finalizada

--- ediaristas/ediaristas/administracao/views/usuario_views.py
+++ ediaristas/ediaristas/administracao/views/usuario_views.py
@@ -1,11 +1,17 @@
 from ..forms.usuario_forms import UsuarioForm
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import get_user_model
 
 def cadastrar_usuario(request):
     if request.method == "POST":
         form_usuario = UsuarioForm(request.POST)
         if form_usuario.is_valid():
             form_usuario.save()
+            return redirect('listar_usuarios')
     else:
         form_usuario = UsuarioForm()
     return render(request,'usuarios/form_usuario.html',{'form_usuario':form_usuario})
+
+def listar_usuarios(request):
+    usuarios = get_user_model().objects.filter(is_superuser=True)
+    return render(request,'usuarios/lista_usuarios.html',{"usuarios": usuarios})
