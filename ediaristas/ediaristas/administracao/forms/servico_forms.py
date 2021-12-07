4fd3f3ade767e50a4c0bacd7cbc4643ff852c855
Rodrigo Belo <rodrigobelosilva@gmail.com>
2021-12-06 22:25:21 -0300

    Cadastro de usuarios finalizado

--- ediaristas/ediaristas/administracao/forms/servico_forms.py
+++ ediaristas/ediaristas/administracao/forms/servico_forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Servico
+from django.forms import widgets
+from ..models import Servico
 from decimal import Decimal
 
 class ServicoForm(forms.ModelForm):
